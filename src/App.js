a0a74e0cbec69a405ac69359b9f11a0e39813369
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-17 18:57:38 -0600

    Fixed flexbox issue with row display

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Pokegame from './Pokegame/Pokegame'
 
--- src/Pokedex/Pokedex.js
+++ src/Pokedex/Pokedex.js
@@ -5,13 +5,11 @@ const Pokedex = ({card1, card2, card3, card4}) => {
 
   return (
     <div className="Pokedex">
-      <div className="PokedexOne">
         <Pokecard pokemon={card1} />
         <Pokecard pokemon={card2} />
         <Pokecard pokemon={card3} />
         <Pokecard pokemon={card4} />
       </div>
-    </div>
   );
 };
 
